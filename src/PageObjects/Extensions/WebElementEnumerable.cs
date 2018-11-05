aff1c8bbd78274077bc6654a34c053482f5628b1
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2018-11-06 00:11:34 +0200

    Fix multiple elements retrieval in Linq methods (#1)

---
+++ src/PageObjects/Extensions/WebElementEnumerable.cs
@@ -0,0 +1,43 @@
+﻿#if NETSTANDARD2_0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumExtras.PageObjects
+{
+    /// <summary>
+    /// Due to Linq optimized execution in dotnet core for IList, some methods lead to multiple elements retrieval.
+    /// In this class IList is wrapped in IEnumerable to disable that 'optimized' evaluation.
+    /// </summary>
+    public static class WebElementEnumerable
+    {
+        public static IEnumerable<TResult> Select<TElement, TResult>(this IList<TElement> webElements, Func<TElement, TResult> selector)
+            => webElements.ToEnumerable().Select(selector);
+
+        public static IEnumerable<TElement> Where<TElement>(this IList<TElement> webElements, Func<TElement, bool> selector)
+            => webElements.ToEnumerable().Where(selector);
+
+        public static List<TElement> ToList<TElement>(this IList<TElement> webElements)
+            => webElements.ToEnumerable().ToList();
+
+        public static TElement[] ToArray<TElement>(this IList<TElement> webElements)
+            => webElements.ToEnumerable().ToArray();
+
+        private static IEnumerable<T> ToEnumerable<T>(this IList<T> enumerable)
+        {
+            IEnumerable<T> ToEnumerableInner(IList<T> e)
+            {
+                foreach (var element in e)
+                    yield return element;
+            }
+
+            return enumerable is WebElementListProxy
+                ? ToEnumerableInner(enumerable)
+                : enumerable;
+        }
+    }
+}
+
+#endif
---
+++ test/PageObjects/WebElementListProxyTests.cs
@@ -0,0 +1,83 @@
+﻿#if NETCOREAPP2_0
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumExtras.PageObjects
+{
+    public class WebElementListProxyTests
+    {
+        private Mock<IWebElement> elementMock;
+        private Mock<ISearchContext> driverMock;
+        private By by;
+
+        [SetUp]
+        public void SetupMocks()
+        {
+            by = By.XPath(".//div");
+
+            elementMock = new Mock<IWebElement>();
+            elementMock.Setup(_ => _.Text).Returns("text");
+
+            driverMock = new Mock<ISearchContext>();
+            driverMock.Setup(_ => _.FindElements(by)).Returns(new ReadOnlyCollection<IWebElement>(Enumerable.Repeat(elementMock.Object, 10).ToList()));
+        }
+
+        [Test]
+        public void LinqSelectShouldRetrieveElementsOnce()
+        {
+            var elements = (IList<IWebElement>)WebElementListProxy.CreateProxy(new DefaultElementLocator(driverMock.Object), new[] { by }, false);
+
+            var texts = elements.Select(e => e.Text).ToList();
+
+            driverMock.Verify(_ => _.FindElements(by), Times.Once);
+        }
+
+        [Test]
+        public void LinqSelectManyShouldRetrieveElementsOnce()
+        {
+            var elements = (IList<IWebElement>)WebElementListProxy.CreateProxy(new DefaultElementLocator(driverMock.Object), new[] { by }, false);
+
+            var texts = elements.SelectMany(e => e.Text).ToList();
+
+            driverMock.Verify(_ => _.FindElements(by), Times.Once);
+        }
+
+        [Test]
+        public void LinqWhereShouldRetrieveElementsOnce()
+        {
+            var elements = (IList<IWebElement>)WebElementListProxy.CreateProxy(new DefaultElementLocator(driverMock.Object), new[] { by }, false);
+
+            var texts = elements.Where(e => e.Text == "a").ToList();
+
+            driverMock.Verify(_ => _.FindElements(by), Times.Once);
+        }
+
+        [Test]
+        public void LinqToListShouldRetrieveElementsOnce()
+        {
+            var elements = (IList<IWebElement>)WebElementListProxy.CreateProxy(new DefaultElementLocator(driverMock.Object), new[] { by }, false);
+
+            var texts = elements.ToList();
+
+            driverMock.Verify(_ => _.FindElements(by), Times.Once);
+        }
+
+        [Test]
+        public void LinqToArrayShouldRetrieveElementsOnce()
+        {
+            var elements = (IList<IWebElement>)WebElementListProxy.CreateProxy(new DefaultElementLocator(driverMock.Object), new[] { by }, false);
+
+            var texts = elements.ToArray();
+
+            driverMock.Verify(_ => _.FindElements(by), Times.Once);
+        }
+    }
+}
+
+#endif
